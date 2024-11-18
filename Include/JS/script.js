2ff5ac9eb67e3bf6a8ee7f68f126f9c20ed9a16a
Aziz865046 <aziz865046@outlook.com>
2024-11-18 23:02:47 +0530

    gender error

--- Include/JS/script.js
+++ Include/JS/script.js
@@ -1,5 +1,7 @@
 // validation script here
 const inputs = document.querySelectorAll("input");
+// const inputs = document.getElementById("form-data").querySelectorAll("input");
+
 const password = document.getElementById("password");
 const cnfrmpassword = document.getElementById("cnfrpassword");
 const btn = document.getAnimations("btn");
@@ -9,12 +11,21 @@ let regForm = document.getElementById("regForm");
 let switchBtn = document.getElementById("showbtn");
 let submitForm = document.getElementById("form-data");
 
+let fname = document.getElementById("name");
+let userName = document.getElementById("username");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let genderItem = document.getElementsByClassName("genderitem");
+let gender = submitForm.elements["gender"];
 const patterns = {
   name: /[a-zA-Z\sa-zA-Z]{4,20}/,
   username: /^[a-zA-Z-\d]{5,12}$/,
-  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  // email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  email:
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,8})(\.[a-zA-Z]{2,8})?$/,
   phone: /^\d{10}$/,
   password: /^[\w@-]{8,20}$/,
+  cnfrpassword: /^[\w@-]{8,20}$/,
 };
 
 function validate(field, regex) {
@@ -54,9 +65,8 @@ function registration() {
   let userName = document.getElementById("username").value;
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phone").value;
-
-  let form = document.getElementById("form-data");
-  let gender = form.elements["gender"].value;
+  let gender = submitForm.elements["gender"].value;
+  console.log(gender);
 
   // Create a new row and cells
   let table = document.getElementById("table");
@@ -67,7 +77,12 @@ function registration() {
   let emailCell = newRow.insertCell(2);
   let phoneCell = newRow.insertCell(3);
   let genderCell = newRow.insertCell(4);
-
+  // Add data to session storage
+  sessionStorage.setItem("name", name);
+  sessionStorage.setItem("username", userName);
+  sessionStorage.setItem("email", email);
+  sessionStorage.setItem("gender", gender);
+  // sessionStorage.setItem("a", "b");
   // Add values to the cells
   nameCell.textContent = name;
   usernameCell.textContent = userName;
@@ -90,20 +105,83 @@ showbtn.addEventListener("click", () => {
 });
 submitForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  validateInputs();
-  registration();
+  console.log("submit");
+  // if (validateInputs()) {
+  // genderCheck();
+  if (validInputs()) {
+    registration();
+    console.log("hello");
+    // submitForm.submit();
+  }
 });
-function validateInputs() {
+// function validateInputs() {
+//   let allValid = true;
+//   // registration();
+//   let gender = submitForm.elements["gender"].value;
+//   inputs.forEach((input) => {
+//     if (!input.value.trim() || !gender) {
+//       allValid = false;
+//     } else if (checkValidity()) {
+//       console.log(!input.checkValidity());
+//     } else {
+//     }
+//   });
+//   console.log(allValid);
+//   if (allValid) {
+//     //   submitForm.submit();
+//     return true;
+//   }
+// }
+
+function checkValidity() {
+  if (patterns.name.test(fname.value)) {
+    return (allValid = false);
+  }
+}
+// isFormValid(email);
+function isFormValid(e) {
+  console.log(e);
+
+  const reg = patterns[e];
+  console.log(reg);
+
+  return reg.test(e);
+}
+function validInputs() {
   let allValid = true;
+
+  console.log("submit");
   inputs.forEach((input) => {
-    if (!input.value.trim()) {
+    // console.log(input);
+    if (input.value.trim() == "") {
+      // console.log(input.value);
       allValid = false;
-    } else if (!input.checkValidity()) {
-      allValid = false;
+
+      console.log("error");
+    } else if (isFormValid(input.id)) {
+      allValid = true;
+      console.log(input.value);
+      console.log("sucess");
     } else {
+      console.log(input.value);
+      allValid = false;
+      console.log("return errors");
     }
   });
-  if (allValid) {
-    submitForm.submit();
+}
+// console.log(document.getElementById("form-data").querySelectorAll("input"));
+function genderCheck() {
+  const male = document.getElementById("male");
+  const female = document.getElementById("female");
+  const other = document.getElementById("prfnot");
+  const error = document.getElementById("genderError");
+  if (!male.checked && !female.checked && !other.checked) {
+    console.log("error");
+    error.style.opacity = "1";
+  } else if (male.checked || female.checked || other.checked) {
+    error.style.opacity = "0";
+    console.log("one select");
+  } else {
+    alert("Valid Input :-)");
   }
 }

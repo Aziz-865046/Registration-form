2800f942069d4c70909bac24a2700bbfd48edebd
Aziz865046 <aziz865046@outlook.com>
2024-11-20 23:18:50 +0530

    localstorage

--- Include/JS/script.js
+++ Include/JS/script.js
@@ -1,7 +1,8 @@
 // validation script here
-const inputs = document.querySelectorAll("input");
-// const inputs = document.getElementById("form-data").querySelectorAll("input");
-
+// const inputs = document.querySelectorAll("input");
+const inputs = document
+  .getElementById("info-section")
+  .querySelectorAll("input");
 const password = document.getElementById("password");
 const cnfrmpassword = document.getElementById("cnfrpassword");
 const btn = document.getAnimations("btn");
@@ -17,6 +18,36 @@ let email = document.getElementById("email");
 let phone = document.getElementById("phone");
 let genderItem = document.getElementsByClassName("genderitem");
 let gender = submitForm.elements["gender"];
+
+function saveData() {
+  // submitForm.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  const increment = document.getElementById("increment");
+  let a = increment.value;
+  const b = a + 1;
+  increment.value = b;
+  const Fd = new FormData(submitForm);
+
+  const obj = Object.fromEntries(Fd);
+  const json = JSON.stringify(obj);
+  localStorage.setItem(`form${b}`, json);
+
+  //////////////////////////////////////////////////////
+  const getjson = localStorage.getItem(`form`);
+  const getobj = JSON.parse(getjson);
+  console.log(getobj);
+  console.log(getobj.name + " : name   ");
+
+  // for (key in obj) {
+  //   const markup = `<div>
+  //           <span>${key}:${obj[key]} </span></div>`;
+  //   document.getElementById("data").innerHTML += markup;
+  // }
+  //////////////////////////////////////////////////////
+  // window.location.href = "./Include/confirm.html";
+  // });
+}
+
 const patterns = {
   name: /[a-zA-Z\sa-zA-Z]{4,20}/,
   username: /^[a-zA-Z-\d]{5,12}$/,
@@ -25,14 +56,15 @@ const patterns = {
     /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,8})(\.[a-zA-Z]{2,8})?$/,
   phone: /^\d{10}$/,
   password: /^[\w@-]{8,20}$/,
-  // cnfrpassword: /^[\w@-]{8,20}$/,
-  gender: /[male]?[female]?[other]?/,
-  // gender: genderCheck(),
+  cnfrpassword: /^[\w@-]{8,20}$/,
+  // gender: /[male]?[female]?[other]?/,
 };
-
 function validate(field, regex) {
+  // field.nextElementSibling.style.opacity = "1";
   if (regex.test(field.value)) {
     field.className = "valid";
+    // field.style.opacity = "1";
+
     return true;
   } else {
     field.className = "invalid";
@@ -68,7 +100,7 @@ function registration() {
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phone").value;
   let gender = submitForm.elements["gender"].value;
-  console.log(gender);
+  // console.log(gender);
 
   // Create a new row and cells
   let table = document.getElementById("table");
@@ -104,14 +136,18 @@ function registration() {
 //     textElement.innerText = "Show Data";
 //   }
 // });
+////////////////////////////////////////////////////////////////
 submitForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("submit");
+  console.log("click submit btn");
   genderCheck();
+  saveData();
   if (validInputs()) {
     // registration();
     console.log("hello");
-    // submitForm.submit();
+    submitForm.submit();
+  } else {
+    console.log("------------------error-----------------");
   }
 });
 //
@@ -123,35 +159,33 @@ function checkValidity() {
 // isFormValid(email);
 function testpattern(e, value) {
   const reg = patterns[e];
-  console.log(reg);
+  // console.log(reg);
   return reg.test(value);
 }
 
 function validInputs() {
-  // let allValid = true;
-  registration();
-  sttorage();
+  let allValid = true;
+  // registration();
+
   inputs.forEach((input) => {
-    console.log("form erro check");
     console.log("data : " + input.dataset.regex);
 
-    // console.log(isFormValid(in));
-    // console.log(isFormValid(input.id));
-    console.log(isFormValid(input.dataset.regex, input.value));
-    // if (input.value.trim() == "") {
-    //   // allValid = false;
+    console.log(testpattern(input.dataset.regex, input.value));
+    if (input.value.trim() == "") {
+      allValid = false;
 
-    //   console.log("error");
-    //   // } else if (isFormValid(input.id)) {
-    // } else if (isFormValid(input.dataset.regex)) {
-    //   // allValid = true;
-    //   console.log(input.value);
-    //   console.log("sucess");
-    // } else {
-    //   console.log(input.value);
-    //   // allValid = false;
-    //   console.log("return errors");
-    // }
+      console.log("error");
+      return false;
+    } else if (testpattern(input.dataset.regex, input.value)) {
+      allValid = true;
+      console.log("sucess");
+      return true;
+    } else {
+      console.log(input.value);
+      allValid = false;
+      console.log("return errors");
+      return false;
+    }
   });
   if (allValid) {
     //   submitForm.submit();
@@ -170,27 +204,27 @@ function genderCheck() {
     error.style.opacity = "0";
   }
 }
-function sttorage() {
-  const arr = [];
-  const arr2 = [];
-  for (let input of inputs) {
-    arr.push(input.value);
-    arr2.push(input.id);
-  }
-  arr2.push("gen");
-  console.log(arr2);
-  for (let i = 0; i < arr.length + 1; i++) {
-    localStorage.setItem(arr2[i], arr[i]);
+// function sttorage() {
+//   const arr = [];
+//   const arr2 = [];
+//   for (let input of inputs) {
+//     arr.push(input.value);
+//     arr2.push(input.id);
+//   }
+//   arr2.push("gen");
+//   console.log(arr2);
+//   for (let i = 0; i < arr.length + 1; i++) {
+//     localStorage.setItem(arr2[i], arr[i]);
 
-    console.log(arr2[i] + " : " + arr[i]);
-  }
-  arr.push(submitForm.elements["gender"].value);
-  console.log(arr2);
-  console.log(arr);
-  console.log(arr[0]);
-  console.log(arr[1]);
-  console.log(arr[2]);
-  console.log(arr[3]);
-  console.log(arr[3]);
-  console.log(arr[9]);
-}
+//     console.log(arr2[i] + " : " + arr[i]);
+//   }
+//   arr.push(submitForm.elements["gender"].value);
+//   console.log(arr2);
+//   console.log(arr);
+//   console.log(arr[0]);
+//   console.log(arr[1]);
+//   console.log(arr[2]);
+//   console.log(arr[3]);
+//   console.log(arr[3]);
+//   console.log(arr[9]);
+// }
